a5516db7df133588136c6ba14d5091842638b972
Meenakshi <hiringhrbot39@gmail.com>
2023-10-08 22:01:01 +0530

    array method splice

--- arrayMethods/script.js
+++ arrayMethods/script.js
@@ -1,76 +1,54 @@
-'use strict';
+// =====================SLICE==========================================================================
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
+/*
+slice method original array mutate cheyilla
 
-// Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
+ath oru new array return cheyyum
+ */
 
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
+const arr =[1,2,3,4]
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
+console.log(arr.slice(1));
 
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
+console.log(arr.slice(1,3));
+/*index 1,2 il ulla element vare display aaku.
+index 3 il ullat console cheyyan pattilla */
 
-const accounts = [account1, account2, account3, account4];
 
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+console.log(arr.slice(-2));
+/*array il ulla last 2 elements matram matiyenkil negative 2 cheytha mati */
+console.log(arr.slice(-1));
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+console.log(arr.slice(0,-2));
+/*arrayile 0th index tott slice cheyyanam and last 2 elements avoid cheyyanam */
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+console.log(arr.slice());
+/*array dae shallow copy create cheyyum */
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+console.log([...arr]);
+/*creating new array[]    and expanding original array into that[...arr]
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+ethum array dae shallow copy create cheyyum.
+*/
 
-/////////////////////////////////////////////////
+
+console.log(arr);
+
+// ============================SPLICE======================================================================
+
+console.log(arr.splice(2));
+
+console.log(arr);
+
+/*splice and slice thammil ulla difference 
+
+splice- will mutate original array
+slice- will not mutate original array
+
+
+*/
+
+console.log(arr.splice(-1));
+/*last il ulla element matram kittum */
